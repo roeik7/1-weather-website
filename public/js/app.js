7d09c35b0d1e6e1b1922c73f4cd507986ad138b5
roeik7 <roika@mta.ac.il>
2019-03-29 18:41:39 +0300

    unneseccery code in js

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,3 @@
-console.log('javascript file')
-// fetch('http://localhost:3000/weather?address=boston').then((response)=>{
-//     response.json().then((data)=>{
-//         if(data.error){
-//             console.log(error)
-//         }
-//         else{
-//             console.log(data.location)
-//             console.log(data.forecast)
-//         }
-//     })
-// })
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
