4a9b70843a5621d76cb6ee9270aa46562f8fbe5e
roeik7 <roika@mta.ac.il>
2019-04-11 18:48:45 +0300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+const port = process.env.PORT || 3000
+
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -88,6 +90,6 @@ app.get('*', (req, res)=>{
     })
 })
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
 
 })
