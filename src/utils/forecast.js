f71b564b6e02d6b7fc4e78a7e57c7db456d85c73
roeik7 <roika@mta.ac.il>
2019-04-11 20:10:19 +0300

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback) =>{
             callback('Unable to find location!', undefined)
         }
         else{
-            callback(undefined, body.daily.data[0].summary + ' It is currently '+ body.currently.temperature + ' degrees out. There is a '+ body.currently.precipProbability+ ' % for a rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently '+ body.currently.temperature + ' degrees out. This is the high temperature today: '+ body.daily.data[0].temperatureHigh + ' and the low: '+ body.daily.data[0].temperatureLow +'.' + 'There is a '+ body.currently.precipProbability+ ' % for a rain.')
         }
     })
 }
